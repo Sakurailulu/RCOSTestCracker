121c4b7d16a77f138834e6972be53e43824b4564
LiyunA <34258282+LiyunA@users.noreply.github.com>
2019-10-15 16:27:28 -0400

    general structure

---
+++ Cracker/app/src/main/java/com/example/cracker/App.java
@@ -0,0 +1,26 @@
+package com.example.cracker;
+
+import android.app.Application;
+import android.content.Context;
+
+import cn.bmob.v3.Bmob;
+
+
+/**
+ * Entity for application
+ * */
+public class App extends Application {
+
+    private static Context mContext;
+
+    @Override
+    //
+    public void onCreate() {
+        super.onCreate();
+        mContext = getApplicationContext();
+        Bmob.initialize(this, "9a2a1c3bd3f4a0cac01abaf4da1164f7");
+    }
+    public static Context getContext(){
+        return mContext;
+    }
+}
