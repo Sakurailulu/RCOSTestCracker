2b38fa451eea7830d3541e0eb74b1366bc3dc98e
Sakurailulu <34151383+Sakurailulu@users.noreply.github.com>
2019-10-15 18:37:47 -0400

    Update AddNoteActivity.java

--- Cracker/app/src/main/java/com/example/cracker/AddNoteActivity.java
+++ Cracker/app/src/main/java/com/example/cracker/AddNoteActivity.java
@@ -19,6 +19,46 @@ public class AddNoteActivity extends BaseActivity  {
 	private String year;
 	private String month;
 	private String day;
+
+	@Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_add);
+        year = getIntent().getStringExtra("year");
+        month = getIntent().getStringExtra("month");
+        day = getIntent().getStringExtra("day");
+        initView();
+        onSetTitle("calendar");
+    }
+
+
+    private void initView() {
+        mEtTitle = findViewById(R.id.et_title);
+        mEtContent = findViewById(R.id.et_content);
+        mBtAdd = findViewById(R.id.bt_add);
+        mBtAdd.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                String title = mEtTitle.getText().toString();
+                String content = mEtContent.getText().toString();
+                if (TextUtils.isEmpty(title)){
+                    onToast("Please enter a title");
+                    return;
+                }
+                if (TextUtils.isEmpty(content)){
+                    onToast("Please enter the content");
+                    return;
+                }
+
+                DBDao.getInstance(AddNoteActivity.this).insertNote(new Note(title,content,year+"-"+month+"-"+day));
+                onToast("Added successfully");
+                finish();
+            }
+
+        });
+        
+    }
+
 	
 
 
