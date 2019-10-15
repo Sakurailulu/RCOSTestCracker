15fc8de4dd9b307dd27352b2c342012957fe4e80
Sakurailulu <34151383+Sakurailulu@users.noreply.github.com>
2019-10-15 16:38:08 -0400

    Create AddNoteActivity.java

---
+++ Cracker/app/src/main/java/com/example/cracker/AddNoteActivity.java
@@ -0,0 +1,12 @@
+package com.example.cracker;
+
+
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.text.TextUtils;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.TextView;
+
+import com.example.cracker.bean.Note;
+import com.example.cracker.utils.DBDao;
